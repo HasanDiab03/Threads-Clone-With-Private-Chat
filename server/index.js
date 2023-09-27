753ee427206e0958534ce8e8ce1700dc449f342e
Hasan Diab <hasandiab.main@gmail.com>
2023-09-27 18:08:46 +0300

    private chat

--- server/index.js
+++ server/index.js
@@ -24,12 +24,12 @@ app.use("/api/messages", messageRoutes);
 
 const port = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-  });
-}
+// const __dirname = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+//   });
+// }
 
 server.listen(port, () => console.log(`Server Started at port: ${port}`));
